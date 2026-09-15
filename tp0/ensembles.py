34a5704061fbef5cdb8c68571b5f99f3c4720f7d
NolannDuriez <duriez.nolann@yahoo.com>
2026-09-15 17:33:31 +0200

    Ex4 partie2

--- tp0/ensembles.py
+++ tp0/ensembles.py
@@ -25,3 +25,23 @@ exploration_seule = robots_exploration_seulement(robots_exploration, robots_tran
 assert double_mission == {"R5", "R7"}
 assert toutes_missions == {"R2", "R3", "R5", "R7", "R9"}
 assert exploration_seule == {"R2"}
+
+
+def ajouter_robot_mission(ensemble_robots, nouveau_robot):
+    """Renvoie un nouvel ensemble de robots avec un robot rajouté"""
+    nouvel_ensemble = ensemble_robots.copy()
+    nouvel_ensemble.add(nouveau_robot)
+    return nouvel_ensemble
+
+def retirer_robot_mission(ensemble_robots, robot_panne):
+    """Renvoie un nouvel ensemble de robots avec un robot retiré"""
+    nouvel_ensemble = ensemble_robots.copy()
+    nouvel_ensemble.remove(robot_panne)
+    return nouvel_ensemble
+
+ajout = ajouter_robot_mission(robots_exploration, "R8")
+retrait = retirer_robot_mission(robots_transport, "R9")
+assert ajout == {"R2", "R5", "R7", "R8"}
+assert retrait == {"R3", "R5", "R7"}
+# L’ensemble d’origine ne doit pas avoir été modifié
+assert robots_transport == {"R5", "R9", "R7", "R3"}
